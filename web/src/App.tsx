e00b6d10b81f24351844f9f763accdd453ee0872
marco-amorim <marcoamorim95@hotmail.com>
2020-10-15 20:45:24 -0300

    initial styling on landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './assets/styles/global.css';
+import './assets/styles/pages/landing.css';
+
+import logoImg from './assets/images/logo.svg';
 
 const App = () => {
-	return <div>Happy</div>;
+	return (
+		<div id="page-landing">
+			<div className="content-wrapper">
+				<img src={logoImg} alt="Happy Logo" />
+
+				<main>
+					<h1>Leve felicidade para o mundo</h1>
+					<p>Visite orfanatos e mude o dia de muitas crianças.</p>
+				</main>
+
+				<div className="location">
+					<strong>Florianópolis</strong>
+					<span>Santa Catarina</span>
+				</div>
+				<a href="/" className="enter-app">
+					<FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default App;
