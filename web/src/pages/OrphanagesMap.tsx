0769d76fd102e4316cf205b66d6ec23e1df719f5
marco-amorim <marcoamorim95@hotmail.com>
2020-10-15 21:38:41 -0300

    styling OrphanagesMap page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../assets/styles/pages/orphanages-map.css';
+
+import marker from '../assets/images/marker.svg';
 
 const OrphanagesMap = () => {
-	return <div>OrphanageMap</div>;
+	return (
+		<div id="page-map">
+			<aside>
+				<header>
+					<img src={marker} alt="Happy Marker" />
+
+					<h2>Escolha um orfanato no mapa</h2>
+					<p>Muitas crianças estão esperando a sua visita {`:)`}</p>
+				</header>
+
+				<footer>
+					<strong>Florianópolis</strong>
+					<strong>Santa Catarina</strong>
+				</footer>
+			</aside>
+
+			<div></div>
+
+			<Link to="" className="create-orphanage">
+				<FiPlus size={32} color="#FFF" />
+			</Link>
+		</div>
+	);
 };
 
 export default OrphanagesMap;
