ce33fb049003a4419eb33e0e0cb4db4ca937fc0e
marco-amorim <marcoamorim95@hotmail.com>
2020-10-15 22:06:35 -0300

    integration with mapbox and react-leaflet

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import '../assets/styles/pages/orphanages-map.css';
 
@@ -23,7 +26,15 @@ const OrphanagesMap = () => {
 				</footer>
 			</aside>
 
-			<div></div>
+			<Map
+				center={[-27.5916402, -48.5854992]}
+				zoom={14}
+				style={{ width: '100%', height: '100%' }}
+			>
+				<TileLayer
+					url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+				/>
+			</Map>
 
 			<Link to="" className="create-orphanage">
 				<FiPlus size={32} color="#FFF" />
