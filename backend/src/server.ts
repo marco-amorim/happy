c98d40f4557e82bdfdbaf11838b7cec3fc61ee9a
marco-amorim <marcoamorim95@hotmail.com>
2020-10-16 17:53:38 -0300

    creating backend with node and typescript

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/hello', (request, response) => {
+	return response.json({ message: 'Hello world' });
+});
+
+app.listen(3333);
