f30a25bc94648dfd25c9943274a144db11b67714
marco-amorim <marcoamorim95@hotmail.com>
2020-10-29 17:59:53 -0300

    leaflet map icon

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import '../assets/styles/pages/orphanages-map.css';
 
 import marker from '../assets/images/marker.svg';
+
+const mapIcon = Leaflet.icon({
+	iconUrl: marker,
+});
 
 const OrphanagesMap = () => {
 	return (
@@ -34,6 +39,7 @@ const OrphanagesMap = () => {
 				<TileLayer
 					url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
 				/>
+				<Marker icon={mapIcon} position={[-27.5916402, -48.5854992]} />
 			</Map>
 
 			<Link to="" className="create-orphanage">
