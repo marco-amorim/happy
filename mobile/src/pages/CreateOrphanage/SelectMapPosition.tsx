9499d978e2c7b979003bd099369657493a78027f
marco-amorim <marcoamorim95@hotmail.com>
2020-11-22 22:36:43 -0300

    formatting code

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -8,63 +8,63 @@ import MapView, { Marker } from 'react-native-maps';
 import mapMarkerImg from '../../images/map-marker.png';
 
 export default function SelectMapPosition() {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  function handleNextStep() {
-    navigation.navigate('OrphanageData');
-  }
+	function handleNextStep() {
+		navigation.navigate('OrphanageData');
+	}
 
-  return (
-    <View style={styles.container}>
-      <MapView 
-        initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-        style={styles.mapStyle}
-      >
-        <Marker 
-          icon={mapMarkerImg}
-          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
-        />
-      </MapView>
+	return (
+		<View style={styles.container}>
+			<MapView
+				initialRegion={{
+					latitude: -27.2092052,
+					longitude: -49.6401092,
+					latitudeDelta: 0.008,
+					longitudeDelta: 0.008,
+				}}
+				style={styles.mapStyle}
+			>
+				<Marker
+					icon={mapMarkerImg}
+					coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
+				/>
+			</MapView>
 
-      <RectButton style={styles.nextButton} onPress={handleNextStep}>
-        <Text style={styles.nextButtonText}>Próximo</Text>
-      </RectButton>
-    </View>
-  )
+			<RectButton style={styles.nextButton} onPress={handleNextStep}>
+				<Text style={styles.nextButtonText}>Próximo</Text>
+			</RectButton>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    position: 'relative'
-  },
+	container: {
+		flex: 1,
+		position: 'relative',
+	},
 
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+	mapStyle: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
+	},
 
-  nextButton: {
-    backgroundColor: '#15c3d6',
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 56,
+	nextButton: {
+		backgroundColor: '#15c3d6',
+		borderRadius: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 56,
 
-    position: 'absolute',
-    left: 24,
-    right: 24,
-    bottom: 40,
-  },
+		position: 'absolute',
+		left: 24,
+		right: 24,
+		bottom: 40,
+	},
 
-  nextButtonText: {
-    fontFamily: 'Nunito_800ExtraBold',
-    fontSize: 16,
-    color: '#FFF',
-  }
-})
+	nextButtonText: {
+		fontFamily: 'Nunito_800ExtraBold',
+		fontSize: 16,
+		color: '#FFF',
+	},
+});
